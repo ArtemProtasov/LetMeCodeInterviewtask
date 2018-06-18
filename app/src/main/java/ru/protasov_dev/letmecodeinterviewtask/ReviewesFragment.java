a400f518ba9b62d3100fb97f07efa149cfcf5e1d
Artyom Protasov <24913408+ArtemProtasov@users.noreply.github.com>
2018-06-18 20:42:44 +0300

    Добавил поля для ввода ключевых слов и даты. При нажатии на поле ввода даты будет появляться диалог выбора

--- app/src/main/java/ru/protasov_dev/letmecodeinterviewtask/ReviewesFragment.java
+++ app/src/main/java/ru/protasov_dev/letmecodeinterviewtask/ReviewesFragment.java
@@ -1,12 +1,31 @@
 package ru.protasov_dev.letmecodeinterviewtask;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ReviewesFragment extends Fragment{
+
+    private EditText keywords;
+    private EditText date;
+    private Calendar Date = Calendar.getInstance();
+
+    private int Year = 2017;
+    private int Month = 12;
+    private int Day = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -18,4 +37,38 @@ public class ReviewesFragment extends Fragment{
                              Bundle savedInstanceState){
         return inflater.inflate(R.layout.reviewes_fragment, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        keywords = getView().findViewById(R.id.keyword);
+        date = getView().findViewById(R.id.data);
+
+        //При клике на поле ввода даты - отображаем диалог выбора даты
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getContext(), d, Date.get(Calendar.YEAR), Date.get(Calendar.MONTH), Date.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+    }
+
+    // установка даты
+    private void setInitialDate() {
+        //Преобразуем с помощью SimpleDateFormat дату в миллисекундах в следующий формат: ГОД/МЕСЯЦ/ДЕНЬ (так задано в ТЗ)
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        //И сразу устанавливаем в поле ввода форматированную дату
+        date.setText(formatter.format(Date.getTimeInMillis()));
+    }
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            Date.set(Calendar.YEAR, year);
+            Date.set(Calendar.MONTH, monthOfYear);
+            Date.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDate();
+        }
+    };
+
 }
