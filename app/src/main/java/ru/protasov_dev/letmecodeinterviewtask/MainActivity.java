1e1bb372886531ad0375ef1abd3cd83074c17696
Artyom Protasov <24913408+ArtemProtasov@users.noreply.github.com>
2018-06-18 17:48:13 +0300

    Добавил табы, как на рисунках в задании

--- app/src/main/java/ru/protasov_dev/letmecodeinterviewtask/MainActivity.java
+++ app/src/main/java/ru/protasov_dev/letmecodeinterviewtask/MainActivity.java
@@ -1,13 +1,40 @@
 package ru.protasov_dev.letmecodeinterviewtask;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Устанавливаем наш кастомный тулбар
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager();
+
+        tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ReviewesFragment(), "Reviewes");
+        adapter.addFragment(new ReviewesFragment(), "Critics");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/ru/protasov_dev/letmecodeinterviewtask/ReviewesFragment.java
@@ -0,0 +1,21 @@
+package ru.protasov_dev.letmecodeinterviewtask;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReviewesFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.reviewes_fragment, container, false);
+    }
+}
